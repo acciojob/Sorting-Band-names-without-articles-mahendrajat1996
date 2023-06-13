ec91d1daf1bf9a9316b59e5b442a261653574bc1
mahendrajat1996 <130475100+mahendrajat1996@users.noreply.github.com>
2023-06-13 15:47:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let obj={};
+function check(str){
+if(str[0]=='T' && str[1]=='h' && str[2]=='e' && str[3]==' '){
+var res=str.slice(4);
+obj[res]=str;
+return res;
+}
+if(str[0]=='A'){
+if(str[1]==' '){
+var res=str.slice(2);
+obj[res]=str;
+return res;
+}
+if(str[1]=='n' && str[2]==' '){
+var res=str.slice(3);
+obj[res]=str;
+return res;
+}
+}
+obj[str]=str;
+return str;
+}
+for(let i=0;i<touristSpots.length;i++){
+touristSpots[i]=check(touristSpots[i]);
+// console.log(touristSpots[i]);
+}
+touristSpots.sort();
+for(let i=0;i<touristSpots.length;i++){
+touristSpots[i]=obj[touristSpots[i]];
+}
+console.log(touristSpots);
 
