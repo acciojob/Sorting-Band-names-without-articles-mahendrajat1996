6e346008e420033d9b4e9a14f8ac8bef177454fe
mahendrajat1996 <130475100+mahendrajat1996@users.noreply.github.com>
2023-07-03 22:41:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,81 @@
-let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-let obj={};
-function check(str){
-if(str[0]=='T' && str[1]=='h' && str[2]=='e' && str[3]==' '){
-var res=str.slice(4);
-obj[res]=str;
-return res;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<title>Sort Without Articles</title>
+<style>
+body {
+margin: 0;
+font-family: sans-serif;
+background: url('https://source.unsplash.com/nDqA4d5NL0k/2000x2000');
+background-size: cover;
+display: flex;
+align-items: center;
+min-height: 100vh;
 }
-if(str[0]=='A'){
-if(str[1]==' '){
-var res=str.slice(2);
-obj[res]=str;
-return res;
+
+#bands {
+list-style: inside square;
+font-size: 20px;
+background: white;
+width: 500px;
+margin: auto;
+padding: 0;
+box-shadow: 0 0 0 20px rgba(0, 0, 0, 0.05);
 }
-if(str[1]=='n' && str[2]==' '){
-var res=str.slice(3);
-obj[res]=str;
-return res;
+
+#bands li {
+border-bottom: 1px solid #efefef;
+padding: 20px;
 }
+
+#bands li:last-child {
+border-bottom: 0;
 }
-obj[str]=str;
-return str;
+
+a {
+color: #ffc600;
+text-decoration: none;
 }
-for(let i=0;i<touristSpots.length;i++){
-touristSpots[i]=check(touristSpots[i]);
-// console.log(touristSpots[i]);
+</style>
+</head>
+<body>
+<ul id="bands"></ul>
+
+<script type="text/javascript">
+// Given array of band names
+let bandNames = ['The Beatles', 'Led Zeppelin', 'Aerosmith', 'The Rolling Stones'];
+
+// Function to sort band names in lexicographic order excluding articles
+function sortBandNames(bandNames) {
+// Regular expression pattern to match articles
+const articlePattern = /^(a|an|the)\s/i;
+
+// Sort the band names in lexicographic order excluding articles
+bandNames.sort((a, b) => {
+const nameA = a.replace(articlePattern, '').toLowerCase();
+const nameB = b.replace(articlePattern, '').toLowerCase();
+return nameA.localeCompare(nameB);
+});
+
+return bandNames;
 }
-touristSpots.sort();
-for(let i=0;i<touristSpots.length;i++){
-touristSpots[i]=obj[touristSpots[i]];
-}
-console.log(touristSpots);
+
+// Sort the band names
+const sortedBandNames = sortBandNames(bandNames);
+
+// Get the <ul> element by its ID
+const bandList = document.getElementById('bands');
+
+// Iterate over the sorted band names and create <li> elements
+sortedBandNames.forEach((bandName) => {
+// Create <li> element
+const listItem = document.createElement('li');
+// Set the text content of the <li> element to the band name
+listItem.textContent = bandName;
+// Append the <li> element to the <ul> element
+bandList.appendChild(listItem);
+});
+</script>
+</body>
+</html>
